82bfca7c710a46b39920758ffc1b5ec0f835ed84
Florian Meneses <florian.meneses@gmail.com>
2021-08-01 01:11:44 -0700

    Established inital api request urls find by ingredient

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,74 +1,84 @@
-// Page Elements
-const contentEl = $('.content');
-const contentTitleEl = $('<h1>');
-const jokeUrl = "food/jokes/random";
+// Spoonacular API Key
 const spoonApiKey = "apiKey=57ad59d77a0f469ab570c2f890ffb990";
 
+// Object to construct Spoonacular Urls
 var spoonacularUrls = {
+  // Known base Urls for various API calls
+  apiUrl: "https://api.spoonacular.com/",
   jokeUrl: "food/jokes/random",
   triviaUrl: "food/trivia/random",
   randomRecipesUrl: "recipes/random",
-  findByIngredientsUrl: `recipes/findByIngredients/?${spoonApiKey}`,
+  findByIngredientsUrl: "recipes/findByIngredients",
 
+  // Create base url request with api key and optional parameters string
+  constructBaseUrl: function (url, paramsString="") {
+    var baseUrl = this.apiUrl;
+    
+    baseUrl += url;
+    baseUrl += `?${spoonApiKey}`
+    baseUrl += paramsString;
+
+    return baseUrl;
+  },
+
+  // Create Request Url for find recipe by ingredients
   findByIngredients: function (ingredients, numberOfRecipes=10, ignorePantry=true) {
     let count = 0;
+    let baseUrl = this.constructBaseUrl(this.findByIngredientsUrl)
 
+    // Add parameters for findByIngredient
     ingredients.forEach(item => {
-      console.log(item)
 
       if (count == 0) {
-        this.findByIngredientsUrl.concat(`ingredients=${item}`);
+        baseUrl += `&ingredients=${item}`;
       } else {
-        this.findByIngredientsUrl.concat(`,+${item}`);
+        baseUrl += `,+${item}`;
       }
       count++;
     })
 
-    console.log(numberOfRecipes)
     if (numberOfRecipes !== 10) {
-      this.findByIngredientsUrl.concat(`&numberOfRecipes=${numberOfRecipes}`)
+      baseUrl += `&numberOfRecipes=${numberOfRecipes}`;
     }
 
-    console.log(ignorePantry)
-    if (ignorePantry) {
-      this.findByIngredientsUrl.concat(`&ignorePantry=${ignorePantry}`)
+    if (!ignorePantry) {
+      baseUrl += `&ignorePantry=${ignorePantry}`
     }
     
-    console.log(this.findByIngredientsUrl);
-    return this.findByIngredientsUrl;
+    return baseUrl;
   },
 }
 
-function apiCall(url, paramsArray = []) {
-  var params = "";
+// Create full request url w/optional additional parameters for a Spoonacular API call
+function apiCall(baseUrl, params = {}) {
+  let paramsString = ""
 
-  if (paramsArray != null)
-    paramsArray.forEach(param => {
-    params.concat(`&${param}`)
-  });
+  if (params != null) {
+    for (let [key, value] of Object.entries(params)) {
+      paramsString += `&${key}=${value}`;
+    }
+  };
 
-  var requestUrl = `https://api.spoonacular.com/${url}?${spoonApiKey}${params}`
+  let requestUrl = baseUrl + paramsString;
 
   return requestUrl;
 }
 
-// Fetch
-const requestUrl = spoonacularUrls.findByIngredients(['apple', 'derp', 'banana'], 15, false)// apiCall(jokeUrl);
+// DEBUG Request call
+const requestUrl = spoonacularUrls.findByIngredients(['apple', 'derp', 'banana'], 15, false)
 console.log(requestUrl)
 
-// fetch(requestUrl).then( function(response) {
-//   if (!response.status == 200) {
-//     // 404 Redirect
-//   }
-//   return response.json();
+fetch(requestUrl).then( function(response) {
+  if (!response.status == 200) {
+    // TODO: 404 Redirect
+  }
+  return response.json();
 
-// }).then( function(data) {
-//   spoon(data);
-// })
+}).then( function(data) {
+  spoon(data);
+})
 
-// function spoon(data) {
-//   // Add joke
-//   contentTitleEl.text(data.text);
-//   contentEl.append(contentTitleEl);
-//   console.log(data)
-// }
+function spoon(data) {
+  // TODO: Expand to handle various API calls
+  console.log(data)
+}
