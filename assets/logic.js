7e2ed1d14cc0b4a144abf5984bc978074d01c867
Mike Rogers <mrogers23458@gmail.com>
2021-08-01 20:47:41 -0700

    added js functionality to dropdown menu so it now properly toggles open and closed

--- assets/logic.js
+++ assets/logic.js
@@ -1,10 +1,18 @@
 //noticed foundation JS is throwing errors in the console, will add as an issue in github
-
+// variable to target drop down menu button
+var menuBtn = document.getElementById('dropBtn')
+//console log to make sure targeting the right element
+console.log(menuBtn)
 // variable to target the find recipe button
 var searchBtn = document.getElementById('search')
 //console log to make sure targeting the right element
 console.log(searchBtn)
+var dropDownMenu = document.querySelector('.dropdown-content')
 
+function printDropMenu(){
+    dropDownMenu.classList.toggle('show')
+    console.log(dropDownMenu)
+}
 //function to redirect to main page
 function goToMain(){
     window.location.href = "./assets/main.html"
@@ -12,3 +20,4 @@ function goToMain(){
 
 //on click runs go to main, so when 'find recipes' button with id 'search' is clicked it re-directs to main content page
 searchBtn.addEventListener('click', goToMain)
+menuBtn.addEventListener('click', printDropMenu)
