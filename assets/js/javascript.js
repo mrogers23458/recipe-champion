03cb389a360382b9e4665e83ad9e0221d1fef08b
Tucker Lance <thlance@ucdavis.edu>
2021-08-07 16:33:09 -0700

    Modified joke call
    Converted joke call to a conditional based on a stored time. Stores the joke for access on index.html load unless time is greater than 24hrs. Added some alerts and a minor style change for width of search bar.  Not sure if I have the current main... testing.

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -69,14 +69,22 @@ var spoonacularUrls = {
 function addJoke() {
   const jokeEl = $('<p>').addClass('joke h3 text-center');
   const jokeRequest = spoonacularUrls.jokeRequest;
-  const jokeRequestUrl = spoonacularUrls.constructBaseUrl(jokeRequest) ;
-
-  apiCall(jokeRequestUrl).then((data) => {
-    var jokeObject = data;
-    jokeEl.text(jokeObject.text)
-  });
-
-  jokeEl.insertBefore(leadEl)
+  const jokeRequestUrl = spoonacularUrls.constructBaseUrl(jokeRequest);
+  //Check if there is already a joke stored, if not do a call, otherwise use stored
+  if (localStorage.getItem('todaysJoke') == null) {
+    apiCall(jokeRequestUrl).then((data) => {
+      jokeObject = data;
+      console.log(jokeObject);
+      jokeEl.text(jokeObject.text);
+      localStorage.setItem('todaysJoke', jokeObject.text);
+      jokeEl.insertBefore(leadEl);
+    });
+  } else {
+    jokeObject = localStorage.getItem('todaysJoke');
+    jokeEl.text(jokeObject);
+    console.log(jokeObject);
+    jokeEl.insertBefore(leadEl);
+  }
 }
 
 // Search By Ingredients
@@ -123,7 +131,7 @@ function populateMainSearch() {
 // Create full request url w/optional additional parameters for a Spoonacular API call
 function apiCall(baseUrl, params = {}) {
   let paramsString = ""
-
+  console.log('Calling spoonacular: ',baseUrl)
   // Add additional params if provided
   if (params != null) {
     for (let [key, value] of Object.entries(params)) {
@@ -136,13 +144,16 @@ function apiCall(baseUrl, params = {}) {
   return fetch(requestUrl).then( function(response) {
     if (!response.status == 200) {
       // TODO: 404 Redirect
+      alert('Spoonacular responded with status 200');
     }
     return response.json();
   
   }).then( function(data) {
     // TODO: Do something neat with data
     // OR pass data to new function to handle various API requests
+    console.log(data);
     processSpoonacularData(data);
+    recipeCardBuild(data);
     return data;
   }).catch((err) => {
     console.log(err);
@@ -151,10 +162,8 @@ function apiCall(baseUrl, params = {}) {
 
 function processSpoonacularData(data) {
   // TODO: Expand to handle various API calls
-  localStorage.setItem('queryArray', JSON.stringify(data));
-  recipeCardBuild(data);
   console.log(data)
-  return data;
+  //return data; //<--don't think this is necessary, unless the call needs it for something
 }
 
 function printDropMenu(){
@@ -185,8 +194,6 @@ function printHistory(){
 historyBox.on('click', function(e){
   let clickValue = e.target.textContent
   searchByIngredients(clickValue)
-
-
 })
 
 /*Rebuilder button for dev purposes (or to keep?), 
@@ -197,28 +204,29 @@ $('#rebuildCards').click( function rebuildCards () {
     savedData = JSON.parse(localStorage.getItem('queryArray'));
     recipeCardBuild(savedData);
   } else {
-    console.log('No saved data');
+    console.log('No saved search data');
   }
 });
 //Build cards when called
 function recipeCardBuild (array) {
-  $('.recipeCard').remove();
-    array.forEach ((element,index,array) => {
-      newRecipeCard = $('<div class="recipeCard" name="recipe '+element.id+'"></div>');
-      newRecipeTitle = $('<h3 class="recipeTitle">'+element.title+'</h3>');
-      newRecipeImage = $('<img class="recipeImage" src='+element.image+'>');
-      newRecipeOl = $('<ol class="ingredientList" name="recipe '+element.id+'"></ol>');
-      newRecipeCard.append(newRecipeTitle, newRecipeImage, newRecipeOl);
-      element.usedIngredients.forEach((ele,i,arr) => {
-        newRecipeIngUsed = $('<li class="usedIngredient" aisle="'+ele.aisle+'">'+ele.originalString+'</li>');
-        newRecipeOl.append(newRecipeIngUsed);
+    localStorage.setItem('queryArray', JSON.stringify(data));
+    $('.recipeCard').remove();
+      array.forEach ((element,index,array) => {
+        newRecipeCard = $('<div class="recipeCard" name="recipe '+element.id+'"></div>');
+        newRecipeTitle = $('<h3 class="recipeTitle">'+element.title+'</h3>');
+        newRecipeImage = $('<img class="recipeImage" src='+element.image+'>');
+        newRecipeOl = $('<ol class="ingredientList" name="recipe '+element.id+'"></ol>');
+        newRecipeCard.append(newRecipeTitle, newRecipeImage, newRecipeOl);
+        element.usedIngredients.forEach((ele,i,arr) => {
+          newRecipeIngUsed = $('<li class="usedIngredient" aisle="'+ele.aisle+'">'+ele.originalString+'</li>');
+          newRecipeOl.append(newRecipeIngUsed);
+        })
+        element.missedIngredients.forEach((ele2,i2,arr2) => {
+          newRecipeIngMiss = $('<li class="missIngredient" aisle="'+ele2.aisle+'">'+ele2.originalString+'</li>');
+          newRecipeOl.append(newRecipeIngMiss);
+        })
+        $('#recipeContainer').append(newRecipeCard);
       })
-      element.missedIngredients.forEach((ele2,i2,arr2) => {
-        newRecipeIngMiss = $('<li class="missIngredient" aisle="'+ele2.aisle+'">'+ele2.originalString+'</li>');
-        newRecipeOl.append(newRecipeIngMiss);
-      })
-      $('#recipeContainer').append(newRecipeCard);
-    })
 }
 
 // Added Button Event Listeners
@@ -228,7 +236,17 @@ findRecipeBtn && findRecipeBtn.on('click', goToMain)
 
 // Add JOTD to landing page
 if (window.location.pathname.endsWith('index.html')) {
-  addJoke();
+  if (localStorage.getItem('day1') == null) { 
+    localStorage.setItem('day1', Date.now());
+    addJoke();
+  } else if (localStorage.getItem('day1') != null) {
+    day1 = localStorage.getItem('day1');
+    day2 = Date.now();
+    if ( (day2-day1) >= 86400 ) { //24 hours = 86400 seconds
+      addJoke();
+    }
+    localStorage.setItem('day1', day2);
+  }
 }
 
 // Functions on switch to Main Page
