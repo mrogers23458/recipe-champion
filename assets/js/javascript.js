54d3ef030500aa7bf9e6208ffb1d56a0dee21aeb
Tucker Lance <thlance@ucdavis.edu>
2021-08-11 19:29:21 -0700

    Added click selector
    To remove the recipe card who's close button is clicked. It gets the button's ID and then uses jquery to search for the recipe card with the same data-id and removes it.

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -257,12 +257,10 @@ class RecipeCard {
     this.buildCard = function buildCard() {
       // Build Card Elements with selected data provided by the Spoonacular API
       let newRecipeCard = $('<div>').addClass("recipeCard").attr('data-id', this.id);
-      let newTitleDiv = $('<div id="cardHeader"><h3 class="'+this.title+'">'+this.title+'</h3><button class ="cardCloseBtt" id="button '+this.id+'">X</button></div>');
-      //let newRecipeTitle = $('<h3>').addClass("recipeTitle").text(this.title);
-      //let newRecipeCloseBtt = $('<button id="button'+this.id+'">X</button>');
+      // New DIV to allow the h3 title and close button to rest on the same line using flex within the already flex column card
+      let newTitleDiv = $('<div id="cardHeader"><h3 class="'+this.title+'">'+this.title+'</h3><button class ="cardCloseBtt" id="'+this.id+'">X</button></div>');
       let newRecipeImage = $('<img>').addClass("recipeImage").attr('src', this.image);
       let newRecipeOl = $('<ol>').addClass("ingredientList").attr('data-id', this.id);
-      //newRecipeCard.append(newRecipeTitle, newRecipeCloseBtt, newRecipeImage, newRecipeOl);
       newRecipeCard.append(newTitleDiv, newRecipeImage, newRecipeOl);
 
       this.usedIngredients.forEach((ele) => {
@@ -389,6 +387,12 @@ recipeCompareContainer && recipeCompareContainer.on('click', (event) => {
   } else if ($('.recipe-compare-card-2').has($(event.target)).length === 1) {
     slotHasFocus = "slot2";
   }
+})
+
+// To remove recipe cards when close button is clicked
+recipeCompareContainer.on('click', '.cardCloseBtt', (event) => {
+  cardCloseTarget = event.target.getAttribute('id');
+  $(".recipeCard[data-id*='"+cardCloseTarget+"']").remove();
 })
 
 // Search from history list
